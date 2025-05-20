4f05a714f4cf7ba39c18bf30cf4e155e6a417c56
Andy Kong <andyking99@gmail.com>
2025-05-21 02:12:30 +0800

    init rp firmware i guess

---
+++ firmware/rp2040_ov2640/main.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "ov2640.h"
+
+const int PIN_LED = 25;
+
+// const int PIN_CAM_SIOC = 5; // I2C0 SCL
+// const int PIN_CAM_SIOD = 4; // I2C0 SDA
+// const int PIN_CAM_RESETB = 2;
+// const int PIN_CAM_XCLK = 3;
+// const int PIN_CAM_VSYNC = 16;
+// const int PIN_CAM_Y2_PIO_BASE = 6;
+
+const int PIN_CAM_SIOC = 15;   // I2C0 SCL
+const int PIN_CAM_SIOD = 14;   // I2C0 SDA
+const int PIN_CAM_RESETB = 13 ;
+const int PIN_CAM_XCLK = 8;
+const int PIN_CAM_VSYNC = 12;
+const int PIN_CAM_Y2_PIO_BASE = 0;  // DATA2 is GPIO0, and DATA0–DATA7 are non-contiguous but we'll map in software
+
+ 
+const uint8_t CMD_REG_WRITE = 0xAA;
+const uint8_t CMD_REG_READ = 0xBB;
+const uint8_t CMD_CAPTURE = 0xCC;
+
+uint8_t image_buf[352*288*2];
+int main() {
+	stdio_init_all();
+
+
+	sleep_ms(1000);
+	printf("Hello, world!1\n");
+	sleep_ms(1000);
+	printf("Hello, world!2\n");
+	sleep_ms(1000);
+	printf("Hello, world!3\n");
+	sleep_ms(1000);
+	printf("Hello, world!4\n");
+	sleep_ms(1000);
+	printf("Hello, world!5\n");
+    printf("Booted!\n");
+
+	gpio_init(26);
+	gpio_set_dir(26, GPIO_OUT);
+	gpio_put(26, 1);  // set GPIO26 HIGH
+	
+	sleep_ms(1000);
+
+    struct ov2640_config config;
+    config.sccb = i2c0;
+    config.pin_sioc = PIN_CAM_SIOC;
+    config.pin_siod = PIN_CAM_SIOD;
+
+    config.pin_resetb = PIN_CAM_RESETB;
+    config.pin_xclk = PIN_CAM_XCLK;
+    config.pin_vsync = PIN_CAM_VSYNC;
+    config.pin_y2_pio_base = PIN_CAM_Y2_PIO_BASE;
+
+    config.pio = pio0;
+    config.pio_sm = 0;
+
+    config.dma_channel = 0;
+    config.image_buf = image_buf;
+    config.image_buf_size = sizeof(image_buf);
+
+    ov2640_init(&config);
+	printf("post OV init !\n");
+
+    ov2640_reg_write(&config, 0xff, 0x01);
+    uint8_t midh = ov2640_reg_read(&config, 0x1C);
+    uint8_t midl = ov2640_reg_read(&config, 0x1D);
+    printf("MIDH = 0x%02x, MIDL = 0x%02x\n", midh, midl);
+
+    // while (true) {
+    //     int cmd = getchar();  // wait for 1 byte from USB serial
+
+    //     if (cmd == EOF) continue;
+
+    //     gpio_put(PIN_LED, !gpio_get(PIN_LED));
+
+    //     if (cmd == CMD_REG_WRITE) {
+    //         int reg = getchar();
+    //         int value = getchar();
+    //         if (reg == EOF || value == EOF) continue;
+
+    //         ov2640_reg_write(&config, (uint8_t)reg, (uint8_t)value);
+
+    //     } else if (cmd == CMD_REG_READ) {
+    //         int reg = getchar();
+    //         if (reg == EOF) continue;
+
+    //         uint8_t value = ov2640_reg_read(&config, (uint8_t)reg);
+    //         putchar(value);
+
+    //     } else if (cmd == CMD_CAPTURE) {
+    //         ov2640_capture_frame(&config);
+
+    //         for (size_t i = 0; i < config.image_buf_size; ++i) {
+    //             putchar(config.image_buf[i]);
+    //         }
+    //     }
+    // }
+
+    return 0;
+}
---
+++ firmware/rp2040_ov2640/ov2640.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include "ov2640.h"
+#include "ov2640_init.h"
+#include "hardware/dma.h"
+#include "hardware/i2c.h"
+#include "hardware/pwm.h"
+#include "image.pio.h"
+
+static const uint8_t OV2640_ADDR = 0x60 >> 1;
+
+void ov2640_init(struct ov2640_config *config) {
+	printf("first line OV init\n");
+	// XCLK generation (~20.83 MHz)
+	gpio_set_function(config->pin_xclk, GPIO_FUNC_PWM);
+	uint slice_num = pwm_gpio_to_slice_num(config->pin_xclk);
+
+	printf("second line OV init\n");
+	// 6 cycles (0 to 5), 125 MHz / 6 = ~20.83 MHz wrap rate
+	pwm_set_wrap(slice_num, 5);
+	pwm_set_gpio_level(config->pin_xclk, 3);
+	pwm_set_enabled(slice_num, true);
+
+	printf("third line OV init\n");
+	// SCCB I2C @ 100 kHz
+	gpio_set_function(config->pin_sioc, GPIO_FUNC_I2C);
+	gpio_set_function(config->pin_siod, GPIO_FUNC_I2C);
+	gpio_pull_up(config->pin_sioc);
+	gpio_pull_up(config->pin_siod);
+	i2c_init(config->sccb, 100 * 1000);
+	sleep_ms(10);
+
+	printf("fourth line OV init\n");
+	// Initialise reset pin
+	gpio_init(config->pin_resetb);
+	gpio_set_dir(config->pin_resetb, GPIO_OUT);
+
+	printf("fifth line OV init\n");
+	// Reset camera, and give it some time to wake back up
+	gpio_put(config->pin_resetb, 0);
+	sleep_ms(100);
+	gpio_put(config->pin_resetb, 1);
+	sleep_ms(100);
+
+	printf("sixth line OV init\n");
+	// Initialise the camera itself over SCCB
+	ov2640_regs_write(config, ov2640_vga);
+	printf("sixth 1 line OV init\n");
+	ov2640_regs_write(config, ov2640_uxga_cif);
+	printf("sixth 2 line OV init\n");
+
+	printf("seventh line OV init\n");
+	// Set RGB565 output mode
+	ov2640_reg_write(config, 0xff, 0x00);
+	printf("seventh1 line OV init\n");
+	ov2640_reg_write(config, 0xDA, (ov2640_reg_read(config, 0xDA) & 0xC) | 0x8);
+	printf("seventh2 line OV init\n");
+
+	printf("eighth line OV init\n");
+	// Enable image RX PIO
+	uint offset = pio_add_program(config->pio, &image_program);
+	image_program_init(config->pio, config->pio_sm, offset, config->pin_y2_pio_base);
+}
+
+void ov2640_capture_frame(struct ov2640_config *config) {
+	dma_channel_config c = dma_channel_get_default_config(config->dma_channel);
+	channel_config_set_read_increment(&c, false);
+	channel_config_set_write_increment(&c, true);
+	channel_config_set_dreq(&c, pio_get_dreq(config->pio, config->pio_sm, false));
+	channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
+	
+	dma_channel_configure(
+		config->dma_channel, &c,
+		config->image_buf,
+		&config->pio->rxf[config->pio_sm],
+		config->image_buf_size,
+		false
+	);
+
+	// Wait for vsync rising edge to start frame
+	while (gpio_get(config->pin_vsync) == true);
+	while (gpio_get(config->pin_vsync) == false);
+
+	dma_channel_start(config->dma_channel);
+	dma_channel_wait_for_finish_blocking(config->dma_channel);
+}
+
+void ov2640_reg_write(struct ov2640_config *config, uint8_t reg, uint8_t value) {
+	uint8_t data[] = {reg, value};
+	i2c_write_blocking(config->sccb, OV2640_ADDR, data, sizeof(data), false);
+}
+
+uint8_t ov2640_reg_read(struct ov2640_config *config, uint8_t reg) {
+	i2c_write_blocking(config->sccb, OV2640_ADDR, &reg, 1, false);
+
+	uint8_t value;
+	i2c_read_blocking(config->sccb, OV2640_ADDR, &value, 1, false);
+
+	return value;
+}
+
+void ov2640_regs_write(struct ov2640_config *config, const uint8_t (*regs_list)[2]) {
+	while (1) {
+		uint8_t reg = (*regs_list)[0];
+		uint8_t value = (*regs_list)[1];
+
+		if (reg == 0x00 && value == 0x00) {
+			break;
+		}
+
+		ov2640_reg_write(config, reg, value);
+
+		regs_list++;
+	}
+}
